1ed280059075e2efa56fc98eae99e37eb1bbfd55
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2022-08-06 22:06:46 -0300

    Agregado Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/img/Bg-Logo/logo.ico";
+import { BsMoon } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div>
+        <Link to="/">
+          <img src={Logo} alt="logo J|C" />
+        </Link>
+        <div>
+          <BsMoon />
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
