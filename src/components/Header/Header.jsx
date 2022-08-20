dece8ee07111f71e0e7a34c2b5f5b373a337bfd9
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2022-08-20 13:10:48 -0300

    Sombra en header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,28 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../../assets/img/Bg-Logo/logo.ico";
 import { BsMoon } from "react-icons/bs";
 
 const Header = () => {
+  const [headerShadow, setHeaderShadow] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 60) {
+      setHeaderShadow(true);
+    } else {
+      setHeaderShadow(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground);
+
+    return () => {
+      window.removeEventListener("scroll", changeBackground);
+    };
+  }, []);
+
   return (
-    <header className="sticky top-0 left-0 z-50 w-full bg-background-bgColor">
+    <header className={headerShadow ? "header active" : "header"}>
       <div className="flex items-center justify-between h-16 py-1 px-[20%]">
         <a href="#">
           <img className="w-14" src={Logo} alt="logo J|C" />
