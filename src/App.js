462a0eace636f5f49fc92bf1691400d3d4bec189
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2022-11-04 20:09:54 -0300

    Elimanado servicios

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home/Home";
 import Nav from "./components/Nav/Nav";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
-import Services from "./components/Services/Services";
+// import Services from "./components/Services/Services";
 import Portafolio from "./components/Portafolio/Portafolio";
 // import Testimonials from "./components/Testimonials/Testimonials";
 import Contact from "./components/Contact/Contact";
@@ -19,7 +19,6 @@ function App() {
       <About />
       <Skills />
       <Portafolio />
-      <Services />
       <Contact />
       <Footer />
     </>
